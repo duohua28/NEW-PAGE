c29d048ac35d0347c0beebe3c7e789d966417e45
duohua28 <Duohua28@gmail.com>
2021-07-31 10:50:00 +0800

    duohua

---
+++ website/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from os import path
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "HELLO"
+
+    return app
