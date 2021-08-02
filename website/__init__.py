9575e448139086a9c3794b539ebe86be824c867e
duohua28 <Duohua28@gmail.com>
2021-08-02 15:15:20 +0800

    master

--- website/__init__.py
+++ website/__init__.py
@@ -7,4 +7,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "HELLO"
 
+    @app.route('/')
+    def home():
+        return "<h1>HELLO</h1>"
+
+    @app.route('/profile')
+    def profile():
+        return "<h1>PROFILE</h1>"
+
     return app
